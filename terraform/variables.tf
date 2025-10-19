6c3f1d5a95c9a43a8a247dec18b8c6a74357f445
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-10-19 16:09:19 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
+variable "aws_account_id" {
+  description = "AWS Account ID"
+}
+
+variable "project_name" {
+  default = "ecommerce-project"
+}
